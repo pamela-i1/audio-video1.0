36da05db8d5b9c51eb0bc57ec7682ec011ce4fcc
pamela-i1 <pamela.villagrana@i1tech.net>
2021-03-03 22:39:53 -0700

    Diseno

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,30 +5,30 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>AVI (pagina de inicio)</title>
-    <link rel="stylesheet" href="..bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.min.css')}}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700">
-    <link rel="stylesheet" href="..fonts/font-awesome.min.css">
-    <link rel="stylesheet" href="..css/Brands.css">
-    <link rel="stylesheet" href="..css/Features-Clean.css">
-    <link rel="stylesheet" href="..css/Footer-Dark.css">
-    <link rel="stylesheet" href="..css/Header-Blue.css">
-    <link rel="stylesheet" href="..css/Navigation-Clean.css">
-    <link rel="stylesheet" href="..css/Navigation-with-Search.css">
-    <link rel="stylesheet" href="..css/styles.css">
-    <link rel="stylesheet" href="..css/Video-Parallax-Background.css">
+    <link rel="stylesheet" href="{{asset('fonts/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/Brands.css')}}">
+    <link rel="stylesheet" href="{{asset('css/Features-Clean.css')}}">
+    <link rel="stylesheet" href="{{asset('css/Footer-Dark.css')}}">
+    <link rel="stylesheet" href="{{asset('css/Header-Blue.css')}}">
+    <link rel="stylesheet" href="{{asset('css/Navigation-Clean.css')}}">
+    <link rel="stylesheet" href="{{asset('css/Navigation-with-Search.css')}}">
+    <link rel="stylesheet" href="{{asset('css/styles.css')}}">
+    <link rel="stylesheet" href="{{asset('css/Video-Parallax-Background.css')}}">
 </head>
 
 <body>
     <div class="container-fluid d-flex video-parallax-container" style="border-bottom-width: 0.5px;">
         <nav class="navbar navbar-light navbar-expand-md fixed-top navigation-clean" style="background: rgba(0,0,0,0.5);border-width: 1.5px;border-color: var(--light);border-bottom-style: solid;border-bottom-color: var(--light);height: 80px;">
-            <div class="container"><img src="..img/Menu.png" style="width: 30px;"><img src="..img/Logotipo%20Blanco%20(Transparencia)%20.png" style="width: 95px;"><img class="d-md-none d-lg-none d-xl-none" src="..img/Carrito.png" style="height: 50px;">
-                <div class="d-none d-print-flex d-md-flex d-lg-flex d-xl-flex justify-content-between align-items-md-center align-items-xl-center"><img src="..img/Carrito.png" style="width: 40px;">
+            <div class="container"><img src="{{asset('images/Menu.png')}}" style="width: 30px;"><img src="{{asset('images/Logotipo%20Blanco%20(Transparencia)%20.png')}}" style="width: 95px;"><img class="d-md-none d-lg-none d-xl-none" src="{{asset('images/Carrito.png')}}" style="height: 50px;">
+                <div class="d-none d-print-flex d-md-flex d-lg-flex d-xl-flex justify-content-between align-items-md-center align-items-xl-center"><img src="{{asset('images/Carrito.png')}}" style="width: 40px;">
                     <h3 class="d-md-flex d-lg-flex" style="font-size: 18px;color: var(--white);font-family: Montserrat, sans-serif;padding: 5px;">Compra Ahora</h3>
                 </div>
             </div>
         </nav><video autoplay="" loop="" muted="">
-            <source src="C:\Users\lenav\Downloads\AVE\Videos\Televisiones.mp4" type="video/mp4" wp-acf="[{'type':'url','name':'video','label':'Video','wrapper':{'width':30}}]" wp-attr="[{'target':'src','replace':'%1'}]">
+            <source src="{{asset('media/Televisiones.mp4')}}" type="video/mp4" wp-acf="[{'type':'url','name':'video','label':'Video','wrapper':{'width':30}}]" wp-attr="[{'target':'src','replace':'%1'}]">
         </video>
         <h1 class="text-capitalize text-center d-xl-flex flex-grow-0 align-self-end mx-auto" style="color: rgb(255,255,255);text-align: center;font-family: Montserrat, sans-serif;padding: 0;border-radius: 2px;border-bottom: 5px solid #f44611;border-top-left-radius: -2px;padding-top: 0px;margin: 70px;"><strong>Venta de televisiones&nbsp;</strong></h1>
     </div>
@@ -36,11 +36,11 @@
         <h4 class="d-xl-flex flex-column" style="text-align: center;font-family: Montserrat, sans-serif;font-size: 18px;padding-top: 30px;">Descubre más<br></h4>
         <h4 style="font-size: 25px;font-family: Montserrat, sans-serif;text-align: center;margin-bottom: 0px;padding-top: 0px;">Las marcas que manejamos:</h4>
         <div class="row text-center" style="padding: 20px;margin: 0px;">
-            <div class="col"><img src="..img/Samsung.png" style="width: 110px;height: 110px;min-width: none;padding: 10px;"><img src="..img/Panasonic.png" style="width: 110px;height: 110px;padding: 10px;"><img src="..img/Hisense.png" style="width: 110px;height: 110px;padding: 10px;"><img src="..img/Insignia.png" style="width: 110px;height: 110px;padding: 10px;"><img src="..img/Vizio.png" style="height: 110px;padding: 10px;"><img src="assets/img/LG.png" style="height: 110px;width: 110px;padding: 10px;"><img src="assets/img/Sharp.png" style="height: 110px;padding: 10px;width: 110px;"><img src="assets/img/Toshiba.png" style="height: 110px;padding: 10px;"></div>
+            <div class="col"><img src="{{asset('images/Samsung.png')}}" style="width: 110px;height: 110px;min-width: none;padding: 10px;"><img src="{{asset('images/Panasonic.png')}}" style="width: 110px;height: 110px;padding: 10px;"><img src="{{asset('images/Hisense.png')}}" style="width: 110px;height: 110px;padding: 10px;"><img src="{{asset('images/Insignia.png')}}" style="width: 110px;height: 110px;padding: 10px;"><img src="{{asset('images/Vizio.png')}}" style="height: 110px;padding: 10px;"><img src="{{asset('images/LG.png')}}" style="height: 110px;width: 110px;padding: 10px;"><img src="{{asset('images/Sharp.png')}}" style="height: 110px;padding: 10px;width: 110px;"><img src="{{asset('images/Toshiba.png')}}" style="height: 110px;padding: 10px;"></div>
         </div>
     </section>
     <section style="text-align: center;background: #f4f4f4;padding: 10px;">
-        <div class="d-flex justify-content-between align-items-center"><img class="d-none d-print-inline d-sm-none d-md-inline d-lg-inline d-xl-inline" src="assets/img/Forma%202.png" style="width: 200px;"><img src="assets/img/TelevisionesCapas.png" style="width: 300px;"><img class="d-none d-sm-inline d-md-inline d-lg-inline d-xl-inline" src="assets/img/Forma%201.png" style="width: 200px;"></div>
+        <div class="d-flex justify-content-between align-items-center"><img class="d-none d-print-inline d-sm-none d-md-inline d-lg-inline d-xl-inline" src="{{asset('images/Forma%202.png')}}" style="width: 200px;"><img src="{{asset('images/TelevisionesCapas.png')}}" style="width: 300px;"><img class="d-none d-sm-inline d-md-inline d-lg-inline d-xl-inline" src="{{asset('images/Forma%201.png')}}" style="width: 200px;"></div>
         <div class="table-responsive text-nowrap" style="font-family: Montserrat, sans-serif;padding: 40px;">
             <table class="table">
                 <thead>
@@ -82,19 +82,19 @@
             </table>
         </div>
     </section>
-    <section style="background: url(&quot;assets/img/Gaming.png&quot;) bottom / cover;height: 300px;border-bottom: 5px solid #f44611 ;">
+    <section style="background: url(&quot;{{asset('images/Gaming.png')}}&quot;) bottom / cover;height: 300px;border-bottom: 5px solid #f44611 ;">
         <div class="text-center" style="padding: 80px;height: 295px;">
             <h2 class="text-capitalize text-center" style="font-family: Montserrat, sans-serif;color: rgb(255,255,255);font-size: 60px;margin-bottom: 0px;padding: 0px;"><strong>GAMING</strong></h2>
             <p style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;text-align: center;">Tecnología&nbsp; &nbsp;|&nbsp; VRR&nbsp; |&nbsp; HDMI 2.2&nbsp; |&nbsp; HGIG</p><button class="btn btn-primary" type="button" style="text-align: center;background: rgba(0,0,0,0.1);border-color: rgb(255, 255, 255);border-top-color: rgb(255,;border-right-color: 255,;border-bottom-color: 255);border-left-color: 255,;font-family: Montserrat, sans-serif;">Más información</button>
         </div>
     </section>
-    <section style="background: url(&quot;assets/img/Gaming.png&quot;) bottom / cover;height: 300px;border-bottom: 5px solid #f44611 ;">
-        <div class="text-center" style="padding: 80px;background: url(&quot;assets/img/Cinema.png&quot;);height: 295px;">
+    <section style="background: url(&quot;{{asset('images/Gaming.png')}}&quot;) bottom / cover;height: 300px;border-bottom: 5px solid #f44611 ;">
+        <div class="text-center" style="padding: 80px;background: url(&quot;{{asset('images/Cinema.png')}}&quot;);height: 295px;">
             <h2 class="text-center" style="font-family: Montserrat, sans-serif;color: rgb(255,255,255);font-size: 60px;margin-bottom: 0px;padding: 0px;"><strong>CINEMA&nbsp;TOTAL</strong></h2>
             <p style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;text-align: center;">Entretenimiento&nbsp; &nbsp;|&nbsp; HDR Pro&nbsp; |&nbsp; Pantallas inteligentes |&nbsp; Real 4K</p><button class="btn btn-primary" type="button" style="text-align: center;background: rgba(0,0,0,0.1);border-color: rgb(255, 255, 255);border-top-color: rgb(255,;border-right-color: 255,;border-bottom-color: 255);border-left-color: 255,;font-family: Montserrat, sans-serif;">Más información</button>
         </div>
     </section>
-    <section style="background: url(&quot;assets/img/Deportes.png&quot;) bottom / cover;height: 295px;border-bottom: 5px none #f44611 ;">
+    <section style="background: url(&quot;{{asset('images/Deportes.png')}}&quot;) bottom / cover;height: 295px;border-bottom: 5px none #f44611 ;">
         <div class="text-center" style="padding: 80px;">
             <h2 class="text-center" style="font-family: Montserrat, sans-serif;color: rgb(255,255,255);font-size: 60px;margin-bottom: 0px;padding: 0px;"><strong>DEPORTES</strong></h2>
             <p style="color: rgb(255,255,255);font-family: Montserrat, sans-serif;text-align: center;">Tecnología&nbsp; &nbsp;|&nbsp; VRR&nbsp; |&nbsp; HDMI 2.2&nbsp; |&nbsp; HGIG</p><button class="btn btn-primary" type="button" style="text-align: center;background: rgba(0,0,0,0.1);border-color: rgb(255, 255, 255);border-top-color: rgb(255,;border-right-color: 255,;border-bottom-color: 255);border-left-color: 255,;font-family: Montserrat, sans-serif;">Más información</button>
@@ -125,12 +125,12 @@
             <p style="font-family: Montserrat, sans-serif;font-size: 12px;">Privacidad&nbsp; &nbsp; |&nbsp; &nbsp; Legal</p>
             <p style="font-family: Montserrat, sans-serif;font-size: 12px;">Copyright ©Audio Video Electrónica&nbsp;</p>
             <p class="copyright" style="font-family: Montserrat, sans-serif;color: var(--white);"></p>
-            <div class="d-flex justify-content-between" style="width: 117px;"><a href="#"><img src="assets/img/Correo.png" style="height: 25px;"></a><a href="#"><img src="assets/img/Instagram%201.png" style="height: 25px;"></a><a href="#"><img src="assets/img/Facebook%201.png" style="height: 25px;"></a></div>
+            <div class="d-flex justify-content-between" style="width: 117px;"><a href="#"><img src="{{asset('images/Correo.png')}}" style="height: 25px;"></a><a href="#"><img src="{{asset('images/Instagram%201.png')}}" style="height: 25px;"></a><a href="#"><img src="{{asset('images/Facebook%201.png')}}" style="height: 25px;"></a></div>
         </div>
     </footer>
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
-    <script src="assets/js/Video-Parallax-Background.js"></script>
+    <script src="{{asset('js/jquery.min.js')}}"></script>
+    <script src="{{asset('bootstrap/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('js/Video-Parallax-Background.js')}}"></script>
 </body>
 
 </html>
